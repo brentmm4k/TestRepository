75f482decfa8c52c19313f16eed00969c6510459
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-12 10:25:23 +0400

    Create Print Statement Task.py

---
+++ Print Statement Task.py
@@ -0,0 +1,42 @@
+print ('Hello World')
+
+print (5+3)
+
+print ('Python','is','fun!')
+
+print ('She said,\'Hello!\'')
+
+print("First line\nSecond line")
+
+print("Item 1\tItem 2\tItem 3")
+
+print("The result is", 42)
+
+print(12345)
+
+print(3.14159)
+
+print("Dollar sign: $")
+
+print("apple", "banana", "cherry", sep=", ") # Prints words seperated by a comma
+
+print("Hello,", end=" ")
+print("World!")
+
+print("Python! " * 3) # Prints 3 "Pythons"
+
+print("\u2764") # Prints a heart symbol
+
+print() #Prints empty line
+
+print("Hello " + "World!")
+
+print(True) # Prints Boolean values
+print(False)
+
+print(1.2e3) # Prints 1200.0
+
+print("""This is a multiline
+print statement.""")
+
+print("This is a backslash: \\")
