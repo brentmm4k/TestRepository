5574ee40e6ff68f3434bbe07bbc19970eb818644
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-16 14:46:37 +0400

    new

---
+++ fstring.py
@@ -0,0 +1,22 @@
+num = 1000000.00
+print (f'{num:,.2f}')
+
+discount = 0.5
+print (f'{discount:.0%}')
+
+num = 123456789
+print (f'{num:,d}')
+
+num = 12345.6789
+print(f'{num:.2e}')
+
+num = 123456789
+print(f'{num:>20.2f}') 
+print(f'{num:<20.2f}')
+print(f'{num:^20.2f}')
+print(f'{num:^10,.2f}')
+
+INTEREST_RATE = 0.069
+
+
+import turtle
--- math_1.py
+++ math_1.py
@@ -1,7 +1,6 @@
 add1 = 20
 add2 = 10
 add3 = 30
-
 sum = add1+add2+add3
 
 print('The sum is',sum)
@@ -17,6 +16,6 @@ print('The difference is',diff)
 mul1 = 67
 mul2 = 12
 
-quo = mul1*mul2
+prod = mul1*mul2
 
-print ('The quotient is',quo)
+print ('The product is',prod)
