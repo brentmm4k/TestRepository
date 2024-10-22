603cc24cb86349b31804f7f911439a329396e7ff
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-22 13:45:38 +0400

    Update ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -4,3 +4,9 @@ elif age >= 60: print('You are old')
 elif age >= 30: print("You are middle aged")
 elif age >= 18: print('You are an adult')
 else: print('You are not and adult')
+
+num = int(input('Enter your number: '))
+if num >= 0:
+     if num == 0:print("The number is zero.")
+     else: print('The number is positive')
+else: print('The number is negative')
