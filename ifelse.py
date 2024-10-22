94fd905e843b9c880a9abae95e406d9d6acacf84
Brent Muli <brentmarvel.muli@gmail.com>
2024-10-22 13:36:34 +0400

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,6 @@
+age = int(input('Enter your age: '))
+if age == 69: print('Nice')   
+elif age >= 60: print('You are old')
+elif age >= 30: print("You are middle aged")
+elif age >= 18: print('You are an adult')
+else: print('You are not and adult')
